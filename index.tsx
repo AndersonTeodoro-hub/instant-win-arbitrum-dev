ce2fa4888384b701ff62e3a25aa66841ba0428a7
AndersonTeodoro-hub <cris7981x@gmail.com>
2025-12-28 12:15:18 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,25 +1,35 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider, createConfig, http } from 'wagmi';
 import { arbitrum } from 'wagmi/chains';
-import { injected } from 'wagmi/connectors';
+import { injected, walletConnect } from 'wagmi/connectors';
 import App from './App';
+
+const projectId = '94daeb9a2c7c766816ab3ea56255520b';
 
 const config = createConfig({
   chains: [arbitrum],
-  connectors: [injected()],
+  connectors: [
+    injected(),
+    walletConnect({ 
+      projectId,
+      metadata: {
+        name: 'INSTANT WIN',
+        description: 'Arbitrum Prize Protocol',
+        url: 'https://instant-win-arbitrum-dev.vercel.app',
+        icons: ['https://instant-win-arbitrum-dev.vercel.app/icon.png']
+      }
+    }),
+  ],
   transports: {
     [arbitrum.id]: http(),
   },
 });
 
 const queryClient = new QueryClient();
-
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Root element not found");
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
